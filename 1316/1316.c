e481c3735ea02d35310883259de22c339edadfd3
rsursu0 <rsursu0@naver.com>
2022-05-24 14:48:21 +0900

    sef

--- 1316/1316.c
+++ 1316/1316.c
@@ -2,19 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 
-int ck_overlap();
-
 int main()
 {
 	int N;
-	char str[100];
+	int result = 0;
 	scanf("%d", &N);
-	char arr = (char **)malloc(sizeof(char*) * (N+1));
+	char arr[100][100];
+	char ck[100][100];
+	for (int i = 0; i < N; i++)
+		scanf("%s", &arr[i]);
 	for (int i = 0; i < N; i++)
 	{
-		scanf("%s", arr[i]);
-
-	for (int i = 0; i < N; i++)
-		printf("%s\n", arr[i]);
+		int count = 0;
+		int len1;
+		int len2;
+		len1 = strlen(arr[i]);
+		for (int j = 0; j < len1; j++)
+		{
+			if (arr[i][j] != arr[i][j+1])
+				ck[i][strlen(ck[i])] = arr[i][j];
+		}
+		len2 = strlen(ck[i]);
+		for (int j = 0; j < len2; j++)
+		{
+			for (int k = 0; k < len2; k++)
+			{
+				if (ck[i][j] == ck[i][k])
+					count++;
+			}
+		}
+		if (len2 == count)
+			result++;
+		if (count != 0)
+			count = 0;
+	}			
+	printf("%d", result);		
 	return (0);
 }
