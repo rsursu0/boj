6644991bf8148ee4e23cb9f6836a65848ccde111
wbae <rsursu0@naver.com>
2022-05-28 12:57:07 +0900

    1

---
+++ 10757/10757.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+		char nu1;
+		char nu2;
+		int i = 0;
+		int j = 0;
+}
--- 17478/17478.c
+++ 17478/17478.c
@@ -2,8 +2,6 @@
 
 void reculsive(int i, int N)
 {
-	if (N == 0)
-			return ;
 	if (i == N+1)
 			return ;
 	if (i == 0)
@@ -15,7 +13,7 @@ void reculsive(int i, int N)
 	{
 			for (int j = 0; j < i*4; j++)
 					printf("_");
-			printf("\"재귀함수는 자기 자신을 호출하는 함수라네.\"\n");
+			printf("\"재귀함수는 자기 자신을 호출하는 함수라네\"\n");
 	}
 	if (i != N)
 	{
@@ -30,15 +28,21 @@ void reculsive(int i, int N)
 			printf("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
 	}
 	reculsive(i+1, N);
-	for (int j = 0; j < i*4; j++)
-			printf("_");
-	printf("라고 답변하였지.\n");
+	if (i > 0)
+	{
+		for (int j = 0; j < i*4; j++)
+				printf("_");
+		printf("라고 답변하였지.\n");
+	}
+	if (i == 0)
+			printf("라고 답변하였지.");
 }
 
 int main()
 {
 	int N;
 	scanf("%d", &N);
-	reculsive(0, N);
+	if (N >= 1 && N <= 50)
+			reculsive(0, N);
 	return (0);
 }
